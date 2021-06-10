431c384895d9af32d30a21209c3e63286d0f0566
dluster03 <81268999+dluster03@users.noreply.github.com>
2021-06-10 10:41:18 -0500

    commit

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
+import { View } from 'react-native';
 import { CAMPSITES } from '../shared/campsites';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES
+            campsites: CAMPSITES,
+            selectedCampsite: null
         };
     }
 
+    onCampsiteSelect(CampsiteId) {
+        this.setState({selectedCampsite: CampsiteId});
+    } 
+
     render() {
-        return <Directory campsites={this.state.campsites} />;
+        return (
+            <View style={{flex: 1}}>
+               <Directory 
+                    campsites={this.state.campsites}
+                    onPress={CampsiteId => this.onCampsiteSelect(CampsiteId)}
+                />
+                <CampsiteInfo
+                    campsite={this.state.campsites.filter(
+                        campsite => campsite.id === this.state.selectedCampsite)[0]}
+                />
+            </View>
+        )
     }
 }
 
