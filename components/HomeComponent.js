f3669446051105dc4984f7f9789c9722d7cddbc4
dluster03 <81268999+dluster03@users.noreply.github.com>
2021-06-13 05:55:47 -0500

    commit

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { CAMPSITES } from '../shared/campsites';
+
+function RenderItem ({item}) {
+    if (item) {
+        return (
+            <Card
+                featuredTitle={item.name}
+                image={require('./image/react-lake.jpg')}
+            >
+                <Text style={{margin: 10}}>
+                    {item.description}
+                </Text>
+            </Card>
+        );
+    }
+    return <View />;
+}
 
 class Home extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            campsites: CAMPSITES,
+            promotions: PROMOTIONS,
+            partners: PARTNERS
+        };
+    }
 
     static navigationOptions = {
         title: 'Home'
@@ -9,9 +35,17 @@ class Home extends Component {
 
     render() {
         return (
-            <View>
-                <Text>Home Component</Text>
-            </View>
+            <ScrollView>
+                <RenderItem
+                    item={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                />
+                <RenderItem
+                    item={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                />
+                <RenderItem
+                    item={this.state.partners.filter(partner => partner.featured)[0]}
+                />
+            </ScrollView>
         );
     }
 }
