7be4d179fb32ed63b9bc88dc25b896949e903067
dluster03 <81268999+dluster03@users.noreply.github.com>
2021-06-16 22:16:18 -0500

    commit

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { CAMPSITES } from '../shared/campsites';
@@ -14,7 +14,7 @@ class Directory extends Component {
 
     static navigationOptions = {
         title: 'Directory'
-    }
+    };
 
     render() {
         const { navigate } = this.props.navigation;
@@ -31,7 +31,7 @@ class Directory extends Component {
 
         return (
             <FlatList
-                data={props.campsites}
+                data={this.state.campsites}
                 renderItem={renderDirectoryItem}
                 keyExtractor={item => item.id.toString()}
             />
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
+import Constants from 'expo-constants';
 import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
@@ -64,10 +65,12 @@ const AppNavigator = createAppContainer(MainNavigator)
 class Main extends Component {
     render() {
         return (
-            <View style={{
-                flex: 1,
-                paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
-            }}>
+            <View 
+                style={{
+                    flex: 1,
+                    paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+                }}
+            >
                 <AppNavigator />
             </View>
         );
