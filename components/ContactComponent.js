d5b659e3cc841c8ac806e3274fcfc024fedea541
dluster03 <81268999+dluster03@users.noreply.github.com>
2021-06-17 00:03:46 -0500

    commit

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,10 @@
-import { Component } from "react";
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { FlatList } from 'react-native';
+import { ListItem } from 'react-native-elements';
+import { View, Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
+import { PROMOTIONS } from '../shared/promotions';
+import { PARTNERS } from '../shared/partners';
 
 class Contact extends Component {
 
